0d82189f8a82ec91561e566d387082b184c47f32
Clementin Boittiaux <Clementin.Boittiaux@ifremer.fr>
2022-06-18 18:08:49 +0200

    cleaning database image import

--- sfm/database.py
+++ sfm/database.py
@@ -1,4 +1,3 @@
-import numpy as np
 from pathlib import Path
 from utils import load_camera, load_pose_priors
 from colmap.scripts.python.database import COLMAPDatabase
@@ -22,16 +21,17 @@ def import_images(database_path: Path, image_path: Path, camera_file: Path, pose
         camera['params'],
         prior_focal_length=True
     )
-    pose_priors = load_pose_priors(pose_prior_file)
+    pose_priors = load_pose_priors(pose_prior_file) if pose_prior_file is not None else {}
     for image_file in image_path.iterdir():
-        prior_t = np.full(3, np.NaN)
-        prior_q = np.full(4, np.NaN)
-        if pose_priors is not None:
-            if image_file.name in pose_priors:
-                prior_t = pose_priors[image_file.name]['prior_t']
+        if image_file.name in pose_priors:
+            prior_t = pose_priors[image_file.name]['prior_t']
+            if 'prior_q' in pose_priors[image_file.name]:
                 prior_q = pose_priors[image_file.name]['prior_q']
+                db.add_image(image_file.name, camera_id, prior_t=prior_t, prior_q=prior_q)
             else:
-                print(f'No pose prior for {image_file.name}.')
-        db.add_image(image_file.name, camera_id, prior_t=prior_t, prior_q=prior_q)
+                db.add_image(image_file.name, camera_id, prior_t=prior_t)
+        else:
+            print(f'No pose prior for {image_file.name}.')
+            db.add_image(image_file.name, camera_id)
     db.commit()
     db.close()
--- sfm/utils.py
+++ sfm/utils.py
@@ -1,5 +1,6 @@
 import yaml
 import numpy as np
+from pathlib import Path
 
 
 def load_camera(camera_file):
@@ -8,28 +9,28 @@ def load_camera(camera_file):
     return camera
 
 
-def load_pose_priors(pose_prior_file):
+def load_pose_priors(pose_prior_file: Path) -> dict:
     """
     File format for GPS data is:
-    filename1 lat1 lon1 alt1 qw1 qx1 qy1 qz1
-    filename2 lat2 lon2 alt2
+    imagename1 lat1 lon1 alt1 qw1 qx1 qy1 qz1
+    imagename2 lat2 lon2 alt2
     ...
     File format for cartesian data is:
     filename1 X1 Y1 Z1 qw1 qx1 qy1 qz1
     filename2 X2 Y2 Z2
     ...
-    where qw, qx, qy, qz are optional quaternions.
+    where qw, qx, qy, qz are optional camera-to-world quaternions.
     :param pose_prior_file: path to prior file.
     :return: priors
     """
-    if pose_prior_file is not None:
-        pose_priors = {}
-        with open(pose_prior_file, 'r') as f:
-            for line in f:
-                line = line.split(' ')
-                prior_t = np.array(list(map(float, line[1:4])))
-                prior_q = np.full(4, np.NaN)
-                if len(line) == 8:
-                    prior_q = np.array(list(map(float, line[4:8])))
-                pose_priors[line[0]] = {'prior_t': prior_t, 'prior_q': prior_q}
-        return pose_priors
+    pose_priors = {}
+    with open(pose_prior_file, 'r') as f:
+        for line in f:
+            line = line.split(' ')
+            assert len(line) == 4 or len(line) == 8, f'File {pose_prior_file} format is invalid.'
+            image_name = line[0]
+            pose_priors[image_name] = {}
+            pose_priors[image_name]['prior_t'] = np.array(list(map(float, line[1:4])))
+            if len(line) == 8:
+                pose_priors[image_name]['prior_q'] = np.array(list(map(float, line[4:8])))
+    return pose_priors
