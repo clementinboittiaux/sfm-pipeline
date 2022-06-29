540c7f6cc6a8276139ac895524313f16551eb77a
Clementin Boittiaux <Clementin.Boittiaux@ifremer.fr>
2022-06-29 15:07:07 +0200

    merge sfm outputs

--- sfm/sfm.py
+++ sfm/sfm.py
@@ -1,7 +1,62 @@
 import subprocess
+from utils import symlink_images
 from pathlib import Path
 from database import create_database, import_images, import_features, import_matches
 from matching import pairs_from_poses, pairs_from_netvlad, superpoint, superglue, netvlad
+
+
+def mapper(
+        colmap_path: Path,
+        database_path: Path,
+        image_dir: Path,
+        model_dir: Path,
+        use_priors: bool = False,
+        is_gps: bool = False,
+        refine_camera: bool = True
+):
+    model_dir.mkdir(parents=True, exist_ok=True)
+    mapper_options = {
+        '--database_path': database_path,
+        '--image_path': image_dir,
+        '--output_path': model_dir
+    }
+    if use_priors:
+        mapper_options['--Mapper.use_prior_motion'] = 1
+        if is_gps:
+            mapper_options['--Mapper.prior_is_gps'] = 1
+            mapper_options['--Mapper.use_enu_coords'] = 1
+    if not refine_camera:
+        mapper_options['--Mapper.ba_refine_focal_length'] = 0
+        mapper_options['--Mapper.ba_refine_principal_point'] = 0
+        mapper_options['--Mapper.ba_refine_extra_params'] = 0
+    subprocess.run([colmap_path, 'mapper', *[str(x) for kv in mapper_options.items() for x in kv]])
+
+
+def bundle(colmap_path: Path, model_dir: Path, bundle_dir: Path):
+    bundle_dir.mkdir(parents=True, exist_ok=True)
+    bundle_options = {
+        '--input_path': model_dir,
+        '--output_path': bundle_dir,
+        '--BundleAdjustment.refine_principal_point': 1,
+        '--BundleAdjustment.max_num_iterations': 200,
+        '--BundleAdjustment.function_tolerance': 0.0000000001
+    }
+    subprocess.run([colmap_path, 'bundle_adjuster', *[str(x) for kv in bundle_options.items() for x in kv]])
+
+
+def align(colmap_path: Path, model_dir: Path, database_path: Path, align_dir: Path, is_gps: bool = False):
+    align_dir.mkdir(parents=True)
+    align_options = {
+        '--input_path': model_dir,
+        '--output_path': align_dir,
+        '--database_path': database_path,
+        '--robust_alignment': 0
+    }
+    if is_gps:
+        align_options['--ref_is_gps'] = 1
+        align_options['--alignment_type'] = 'enu'
+    subprocess.run([colmap_path, 'model_aligner', *[str(x) for kv in align_options.items() for x in kv]])
+
 
 
 def run_sfm(
@@ -25,16 +80,15 @@ def run_sfm(
     :param pose_prior_path: path to pose prior file.
     :param output_dir: path to output directory.
     """
-    assert not output_dir.exists(), f'Output directory {output_dir} already exists.'
+    output_dir.mkdir(parents=True, exist_ok=False)
+    symlink_images(image_dir, output_dir / 'images')
 
     database_path = output_dir / 'database.db'
     pairs_path = output_dir / 'pairs.txt'
     features_path = output_dir / 'features.h5'
     matches_path = output_dir / 'matches.h5'
     model_dir = output_dir / 'model'
-    adjusted_model_dir = model_dir / 'adjusted'
-
-    adjusted_model_dir.mkdir(parents=True)
+    bundle_dir = model_dir / 'bundle'
 
     create_database(database_path)
     import_images(database_path, image_dir, camera_path, pose_prior_path)
@@ -58,40 +112,48 @@ def run_sfm(
     import_features(database_path, features_path)
     import_matches(database_path, pairs_path, matches_path)
 
-    mapper_options = {
-        '--database_path': database_path,
-        '--image_path': image_dir,
-        '--output_path': model_dir
-    }
-    if use_priors_for_ba:
-        mapper_options['--Mapper.use_prior_motion'] = 1
-        if is_gps:
-            mapper_options['--Mapper.prior_is_gps'] = 1
-            mapper_options['--Mapper.use_enu_coords'] = 1
-    subprocess.run([colmap_path, 'mapper', *[str(x) for kv in mapper_options.items() for x in kv]])
-
-    bundle_options = {
-        '--input_path': model_dir / '0',
-        '--output_path': model_dir / 'adjusted',
-        '--BundleAdjustment.refine_principal_point': 1,
-        '--BundleAdjustment.max_num_iterations': 200,
-        '--BundleAdjustment.function_tolerance': 0.00000001
-    }
-    subprocess.run([colmap_path, 'bundle_adjuster', *[str(x) for kv in bundle_options.items() for x in kv]])
-
+    mapper(colmap_path, database_path, image_dir, model_dir, use_priors=use_priors_for_ba, is_gps=is_gps)
+    bundle(colmap_path, model_dir / '0', bundle_dir)
     if align_model_to_priors:
-        aligned_model_dir = model_dir / 'aligned'
-        aligned_model_dir.mkdir()
-        align_options = {
-            '--input_path': adjusted_model_dir,
-            '--output_path': aligned_model_dir,
-            '--database_path': database_path,
-            '--robust_alignment': 0
-        }
-        if is_gps:
-            align_options['--ref_is_gps'] = 1
-            align_options['--alignment_type'] = 'enu'
-        subprocess.run([colmap_path, 'model_aligner', *[str(x) for kv in align_options.items() for x in kv]])
+        align(colmap_path, bundle_dir, database_path, model_dir / 'align', is_gps=is_gps)
+
+
+def merge_sfms(colmap_path: Path, sfm_dirs: list[Path], output_dir: Path):
+    output_dir.mkdir(parents=True, exist_ok=False)
+
+    image_dir = output_dir / 'images'
+    database_path = output_dir / 'database.db'
+    pairs_path = output_dir / 'pairs.txt'
+    features_path = output_dir / 'features.h5'
+    matches_path = output_dir / 'matches.h5'
+    model_dir = output_dir / 'model'
+
+    create_database(database_path)
+
+    for sfm_dir in sfm_dirs:
+        # Registration saved in model/registrated
+        import_images(
+            database_path,
+            sfm_dir / 'images',
+            sfm_dir / 'registrated' / 'cameras.bin',
+            sfm_dir / 'registrated' / 'images.bin'
+        )
+        symlink_images(sfm_dir / 'images', image_dir)
+
+    pairs_from_poses(
+        database_path,
+        pairs_path,
+        max_pairs=50,
+        max_dist=3,
+        max_angle=45
+    )
+
+    superpoint(image_dir, features_path)
+    superglue(pairs_path, features_path, matches_path)
+    import_features(database_path, features_path)
+    import_matches(database_path, pairs_path, matches_path)
+
+    mapper(colmap_path, database_path, image_dir, model_dir, refine_camera=False)
 
 
 if __name__ == '__main__':
--- sfm/utils.py
+++ sfm/utils.py
@@ -1,15 +1,23 @@
 import yaml
 import numpy as np
 from pathlib import Path
+from colmap.scripts.python import read_write_model
 
 
 def load_camera(camera_path: Path) -> dict:
-    with open(camera_path, 'r') as f:
-        camera = yaml.safe_load(f)
-    return camera
+    if camera_path.suffix == '.txt':
+        with open(camera_path, 'r') as f:
+            camera = yaml.safe_load(f)
+        return camera
+    elif camera_path.suffix == '.bin':
+        cameras = list(read_write_model.read_cameras_binary(camera_path))
+        assert len(cameras) == 1, f'Invalid camera file: {camera_path} has {len(cameras)} cameras.'
+        return cameras[0]._asdict()
+    else:
+        raise Exception(f'Invalid suffix for {camera_path}, must be `.txt` or `.bin`.')
 
 
-def load_pose_priors(pose_prior_path: Path) -> dict:
+def load_pose_priors_txt(pose_prior_path: Path) -> dict:
     """
     File format for GPS data is:
     imagename1 lat1 lon1 alt1 qw1 qx1 qy1 qz1
@@ -36,6 +44,31 @@ def load_pose_priors(pose_prior_path: Path) -> dict:
     return pose_priors
 
 
+def load_pose_priors_colmap(pose_prior_path: Path) -> dict:
+    images = read_write_model.read_images_binary(pose_prior_path)
+    pose_priors = {}
+    for image in images.values():
+        prior_q = image.qvec.copy()
+        if prior_q[0] < 0:
+            prior_q[0] = -prior_q[0]
+        else:
+            prior_q[1:] = -prior_q[1:]
+        pose_priors[image.name] = {
+            'prior_t': -image.qvec2rotmat().T @ image.tvec,
+            'prior_q': prior_q
+        }
+    return pose_priors
+
+
+def load_pose_priors(pose_prior_path: Path) -> dict:
+    if pose_prior_path.suffix == '.txt':
+        return load_pose_priors_txt(pose_prior_path)
+    elif pose_prior_path.suffix == '.bin':
+        return load_pose_priors_colmap(pose_prior_path)
+    else:
+        raise Exception(f'Invalid suffix for {pose_prior_path}, must be `.txt` or `.bin`.')
+
+
 def angle_between_quaternions(q, r):
     """
     Returns the angle in radians between quaternion `q` and quaternions `r`.
@@ -44,3 +77,9 @@ def angle_between_quaternions(q, r):
     :return: angular distance between `q` and `r`.
     """
     return 2 * np.arccos(np.abs(np.sum(q * r, axis=1)).clip(0, 1))
+
+
+def symlink_images(image_dir: Path, output_dir: Path):
+    output_dir.mkdir(parents=True, exist_ok=True)
+    for image_path in image_dir.iterdir():
+        (output_dir / image_path.name).symlink_to(image_path.resolve())
